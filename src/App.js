487bd4c7a4b8f9620887c0b7f12cd225b2c55cde
KhallilB <khallil.bailey@gmail.com>
2019-06-01 11:39:09 -0700

    Home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 // Components
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import Signup from './components/auth/signup/Signup';
+import Login from './components/auth/login/Login';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <BrowserRouter>
+          <div class="Navbar">
+            <Navbar />
+          </div>
+
+          {/* Router */}
+          <div>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/signup" exact component={Signup} />
+              <Route path="/login" exact component={Login} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
