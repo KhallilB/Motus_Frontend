9d09c692ea31c2907b8aca374a8520a9231fc0ce
KhallilB <khallil.bailey@gmail.com>
2019-06-01 10:39:34 -0700

    signup component

---
+++ src/components/auth/signup/Signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Signup.css';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Signup Works</h1>
+      </div>
+    );
+  }
+}
+
+export default Signup;
