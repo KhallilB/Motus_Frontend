eadaeb50372451e02276ddec56d6027590eb9329
KhallilB <khallil.bailey@gmail.com>
2019-06-01 10:13:13 -0700

    import Navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 // Components
-import Navbar from './components/navbar';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
